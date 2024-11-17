ea6c0ddd12d4c800b89ec4a8dcae0a270d3a6a86
Luis Felipe Silva <luisphelipe1000@gmail.com>
2024-11-17 00:04:03 -0300

    Delete src/services/firebaseConfig.jsx

--- src/services/firebaseConfig.jsx
+++
@@ -1,9 +0,0 @@
-
-export default  {
-    apiKey: "AIzaSyA_dDTgTsDaIYzsto3qi6waIZDx9Myp_0M",
-    authDomain: "gchat-e595e.firebaseapp.com",
-    projectId: "gchat-e595e",
-    storageBucket: "gchat-e595e.appspot.com",
-    messagingSenderId: "507835597296",
-    appId: "1:507835597296:web:c7c41ab8c9149bd9a1dd35"
-  }
